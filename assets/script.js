7de719568441a7b67b8fae846f2104cdf38d1cca
Brian Lee <brianlee@Brians-MBP.hsd1.ca.comcast.net>
2020-09-18 21:41:31 -0700

    generate pass/length prompt

--- assets/script.js
+++ assets/script.js
@@ -5,10 +5,19 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//this function generates the password
+function generatePassword(){
+  //getting length of password
+  var promptLength = confirm("There should be a set length for my password");
+    if(promptLength){
+      var passLength = prompt("Enter a number (8 - 128) for character count");
+    }
+}
+
