b22a38bd5739e98c23d1985c1b3e4807e14422b2
Brian Lee <brianlee@Brians-MBP.hsd1.ca.comcast.net>
2020-09-18 21:52:09 -0700

    global var defined and prompted

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var promptChar;
+var promptLength;
+var passChar;
+var passLength;
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -15,9 +19,15 @@ generateBtn.addEventListener("click", writePassword);
 //this function generates the password
 function generatePassword(){
   //getting length of password
-  var promptLength = confirm("There should be a set length for my password");
+  promptLength = confirm("There should be a set length for my password");
     if(promptLength){
-      var passLength = prompt("Enter a number (8 - 128) for character count");
+      passLength = prompt("Enter a number (8 - 128) for character count");
+    }
+  var promptChar = confirm("There should be certain characters included for my password");
+    if(promptChar){
+      alert("Enter all letters that apply");
+      passChar =  prompt("Lowercase (L), Uppercase (U), Numbers (N), Special Characters (S)");
+      passChar = passChar.toLowerCase();
     }
 }
 
